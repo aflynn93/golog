a311702dfdd399fabcbfd176e8cfbe02a085a1b8
aflynn93 <43562703+aflynn93@users.noreply.github.com>
2022-02-20 15:08:00 -0600

    Update defs.go

--- defs.go
+++ defs.go
@@ -1,8 +1,11 @@
 package golog
 
 const (
-	ERROR = "ERROR"
-	FATAL = "FATAL"
+	ERROR   = "ERROR"
+	FATAL   = "FATAL"
+	INFO    = 1
+	DEBUG   = 2
+	VERBOSE = 3
 )
 
 // Initialize a GoLogger with a log level to be able to log messages depending on the configured log level
